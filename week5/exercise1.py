6980b24652458021f207c4ce2355e7fd01f06032
s.bojceska <s.bojceska@student.unsw.edu.au>
2017-06-14 11:39:58 +1000

    refreshed

--- week5/exercise1.py
+++ week5/exercise1.py
@@ -26,44 +26,22 @@ you'll need to figure out for yourself what to do.
 
 from __future__ import division
 from __future__ import print_function
-
-
-# This is a terrible function. The rest of the functions in this file do a
-# much better job of what it's trying to do. Once you've has a little look,
-# move on, and eventually delete this function. (And this comment!)
-# def do_bunch_of_bad_things():
-#     print("Getting ready to start in 9")
-#     print("Getting ready to start in 8")
-#     print("Getting ready to start in 7")
-#     print("Getting ready to start in 6")
-#     print("Getting ready to start in 5")
-#     print("Getting ready to start in 4")
-#     print("Getting ready to start in 3")
-#     print("Getting ready to start in 2")
-#     print("Getting ready to start in 1")
-#     print("Let's go!")
-#
-#     triangle = {"base": 3, "height": 4}
-#     triangle["hypotenuse"] = triangle["base"]**2 + triangle["height"]**2
-#     print("area = " + str((triangle["base"] * triangle["height"])/2))
-#     print("side lengths are:")
-#     print("base: {}".format(triangle["base"]))
-#     print("height: {}".format(triangle["height"]))
-#     print("hypotenuse: {}".format(triangle["hypotenuse"]))
-#
-#     another_hyp = 5**2 + 6**2
-#     print(another_hyp)
-#
-#     yet_another_hyp = 40**2 + 30**2
-#     print(yet_another_hyp)
-
-
-# return a lit of countdown messages, much like in the bad function above.
 # It should say something different in the last message.
+
+
 def countdown(message, start, stop, completion_message):
-    for i in range(start, stop, -1):
-        print(message + str(i))
-    print (completion_message)
+    """Printer coundown message."""
+    cntdwn_list = []
+    if start > stop:
+        step = -1
+    elif start == stop:
+        return(completion_message)
+    else:
+        step = 1
+    for i in range(start, stop, step):
+        cntdwn_list.append(message + " {}".format(i))
+    cntdwn_list.append(completion_message)
+    return(cntdwn_list)
 
 
 # TRIANGLES
@@ -76,24 +54,28 @@ def countdown(message, start, stop, completion_message):
 # The stub functions are made for you, and each one is tested, so this should
 # hand hold quite nicely.
 def calculate_hypotenuse(base, height):
-    hypotenuse  = math.sqrt(base**2 + height**2)
-    return hypotenuse
+    """Calculator hypothenuse by using the math fomula."""
+    hypotenuse = math.sqrt(base**2 + height**2)
+    return(hypotenuse)
 
 
 def calculate_area(base, height):
+    """Calculator area by using the formula."""
     area = (base*height)/2
     return area
 
 
 def calculate_perimeter(base, height):
+    """Calculator perimeter."""
     perimeter = base + height + calculate_hypotenuse(base, height)
     return perimeter
 
 
 def calculate_aspect(base, height):
+    """Calculator aspect."""
     if height > base:
         return "tall"
-    elif height ==base:
+    elif height == base:
         return "equal"
     else:
         return "wide"
@@ -102,13 +84,14 @@ def calculate_aspect(base, height):
 # Make sure you reuse the functions you've already got
 # Don't reinvent the wheel
 def get_triangle_facts(base, height, units="mm"):
+    """Helper Triangle facts."""
     return {"area": calculate_area(base, height),
             "perimeter": calculate_perimeter(base, height),
             "height": height,
             "base": base,
             "hypotenuse": calculate_hypotenuse(base, height),
             "aspect": calculate_aspect(base, height),
-            "units": mm}
+            "units": units}
 
 
 # this should return a multi line string that looks a bit like this:
@@ -128,6 +111,7 @@ def get_triangle_facts(base, height, units="mm"):
 # but with the values and shape that relate to the specific
 # triangle we care about.
 def tell_me_about_this_right_triangle(facts_dictionary):
+    """Return a dictionary used for facts."""
     tall = """
             {height}
             |
@@ -176,41 +160,47 @@ def triangle_master(base,
                     height,
                     return_diagram=False,
                     return_dictionary=False):
+    """Return a dict, diagram, or dict and diagram."""
+    dictionary = get_triangle_facts(base, height, units="mm")
+    diagram = tell_me_about_this_right_triangle(dictionary)
     if return_diagram and return_dictionary:
-        return {'diagram': diagram, 'facts': dictionary}
+        return {"diagram": diagram, "facts": dictionary}
     elif return_diagram:
         return diagram
     elif return_dictionary:
-        return {'facts': dictionary}
+        return {"facts": dictionary}
     else:
         print("You're an odd one, you don't want anything!")
 
 
 def wordy_pyramid():
+    """Helper Pyramid lists."""
     pyramid_list = list_of_words_with_lengths(range(3, 21, 2))
     pyramid_list = list_of_words_with_lengths(range(20, 3, -2))
 
 
 def get_a_word_of_length_n(length):
+    """Getter for a word of length from an URL."""
     import requests
-    try:
-        length = int(length)
-        if length >= 3:
-            baseURL = "http://www.setgetgo.com/randomword/get.php?len="
-            url = baseURL + str(length)
-            r = requests.get(url)
-            message = r.text
-            return message
-    except ValueError:
-        message = None
+    baseURL = "http://www.setgetgo.com/randomword/get.php?len="
+    if length == 0:
+        return(None)
+    elif type(length) != int:
+        return(None)
+    else:
+        url = baseURL + str(length)
+    r = requests.get(url)
+    message = r.text
+    return(message)
 
 
 def list_of_words_with_lengths(list_of_lengths):
-    word.list = []
+    """Looper for list of words with lenghts."""
+    words_list = []
     for length in list_of_lengths:
-        word_list.append(get_a_word_of_length_n(length))
-    return word_list
+        words_list.append(get_a_word_of_length_n(length))
+    return words_list
 
 
 if __name__ == "__main__":
-    do_bunch_of_bad_things()
+    pass
